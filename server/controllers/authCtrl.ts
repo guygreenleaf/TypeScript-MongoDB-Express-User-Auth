3fa2b12770f9eae6877c9d088570b2581ee92e4c
guygreenleaf <yaweh23@gmail.com>
2021-06-02 13:16:47 -0700

    removed unnecessary cookie check

--- server/controllers/authCtrl.ts
+++ server/controllers/authCtrl.ts
@@ -115,14 +115,8 @@ const authCtrl = {
       },
     logout: async(req: Request, res: Response) => {
         try{
-            if(getCookie !== null){
             res.clearCookie('refreshtoken', {path: `/api/refresh_token`});
-
             return res.json({msg: "Logged out."});
-            }
-            else{
-                return res.json({msg: "User is already logged out."});
-            }
         }
         catch (error){
             return res.status(500).json({msg: error.message});
@@ -159,37 +153,5 @@ const loginUser = async (user: UserInterface | null, password: string, res: Resp
 
 };
 
-
-
-function getCookie( name: string ) {
-    var dc,
-        prefix,
-        begin,
-        end;
-    
-    dc = document.cookie;
-    prefix = name + "=";
-    begin = dc.indexOf("; " + prefix);
-    end = dc.length; // default to end of the string
-
-    // found, and not in first position
-    if (begin !== -1) {
-        // exclude the "; "
-        begin += 2;
-    } else {
-        //see if cookie is in first position
-        begin = dc.indexOf(prefix);
-        // not found at all or found as a portion of another cookie name
-        if (begin === -1 || begin !== 0 ) return null;
-    } 
-
-    // if we find a ";" somewhere after the prefix position then "end" is that position,
-    // otherwise it defaults to the end of the string
-    if (dc.indexOf(";", begin) !== -1) {
-        end = dc.indexOf(";", begin);
-    }
-
-    return decodeURI(dc.substring(begin + prefix.length, end) ).replace(/\"/g, ''); 
-}
 
 export default authCtrl;
